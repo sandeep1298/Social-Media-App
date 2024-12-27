cf7509727e5e059c0d8df201839ac4f7a7f26dea
sandy1298 <sandeepm1298@gmail.com>
2024-12-27 16:43:27 +0530

    feat: added vercel json file in client

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ mongoose.connect(MONGOURI)
     });
 
     app.use(cors({
-        origin: ['https://social-media-app-3h5q.vercel.app', 'https://social-media-app-3h5q-git-main-sandeeps-projects-22864139.vercel.app'], 
+        origin: ['https://social-media-app-3h5q.vercel.app', 'https://social-media-app-3h5q-git-main-sandeeps-projects-22864139.vercel.app',"http://localhost:5173"], 
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
         credentials: true, // Allow cookies if needed
     }));
