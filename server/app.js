6bbcd742622b5476245538e3b782d947ad851880
sandy1298 <sandeepm1298@gmail.com>
2024-12-27 15:34:46 +0530

    added vercel.json file

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ mongoose.connect(MONGOURI)
     });
 
     app.use(cors({
-        origin: ['https://social-media-app-3h5q.vercel.app/'], 
+        origin: ['https://social-media-app-3h5q.vercel.app'], 
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
         credentials: true, // Allow cookies if needed
     }));
